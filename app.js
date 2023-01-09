945d419cffcd4ec278e4c47b38a0209a6341626a
fsn <ndzomgafs@gmail.com>
2023-01-09 01:44:21 +0100

    final version etch a sketch

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
-
-const container = document.querySelector('.grid-container');
-
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement('div');
-  div.classList.add('grid-item');
-  container.appendChild(div);
-}
-
-const gridItems = document.querySelectorAll('.grid-item');
+function run(size=16){
+   
+    const container = document.querySelector('.grid-container');
+    container.innerHTML = '';
  
-gridItems.forEach(item => {
-  item.addEventListener('mouseenter', () => {
-    item.classList.add('hovered');
+    for (let i = 0; i < size*size; i++) {
+      const div = document.createElement('div');
+      div.classList.add('grid-item');
+      container.appendChild(div);
+    }
+    container.style.gridTemplateColumns = `repeat(${size}, 25px)`;
+    container.style.gridTemplateRows = `repeat(${size}, 25px)`;
+   
+        const gridItems = document.querySelectorAll('.grid-item');
+   
+        gridItems.forEach(item => {
+          item.addEventListener('mouseenter', () => {
+            item.classList.add('hovered');
+          });
+       
+        });
+   
+}
+ 
+ 
+ 
+const newGridButton = document.querySelector('#new-grid-button');
+ 
+newGridButton.addEventListener('click', () => {
+  const size = prompt('Enter the number of squares per side for the new grid (up to 100):');
+  if (size > 100) {
+    alert('The maximum number of squares per side is 100.');
+    return;
+  }
+  run(size)
+ 
+ 
   });
  
-});
+  run()
+ 
